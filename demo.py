534f1c18f836c6c5bb781bdb0b2532bdec9d660a
pjwilcoxen <wilcoxen@syr.edu>
2020-03-04 07:42:05 -0500

    Add demo

--- demo.py
+++ demo.py
@@ -1,4 +1,31 @@
 #! /bin/python3
 #  Spring 2020 (PJW)
 
+import pandas as pd
 
+incomes = pd.read_csv('state-data.csv',index_col='name')
+geocodes = pd.read_csv('state-geocodes.csv',index_col='Name')
+
+#%%
+
+incomes['reg'] = geocodes['Region']
+incomes['div'] = geocodes['Division']
+incomes['fips'] = geocodes['State FIPS']
+
+grouped = incomes.groupby(['reg','div']) 
+
+grouped_pop = grouped['pop']
+
+totals = grouped_pop.sum()/1e6
+
+print(totals.round(1))
+
+#%%
+
+print( totals.index )
+
+print( '\nRegion: Northeast')
+print( totals[1] )
+
+print( '\nDivision: New England')
+print( totals[:,1] )
