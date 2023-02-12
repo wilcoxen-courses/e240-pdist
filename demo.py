86a3fa6ca5755234f57b6e46f8d6dbf6acbeb495
pjwilcoxen <wilcoxen@syr.edu>
2023-02-12 16:04:08 -0500

    Expand groupby examples

--- demo.py
+++ demo.py
@@ -20,7 +20,7 @@ county = pd.read_csv('county_pop.csv',dtype=fips)
 #  and then rename the population column to make it easier to read.
 #
 
-county = county.set_index(['state','county'])
+county = county.set_index(['state','county', 'NAME'])
 county = county.rename(columns={'B01001_001E':'pop'})
 
 #%%
@@ -81,5 +81,106 @@ print( county.xs('04',level='state') )
 #
 #  Check that each state's counties add up to 100%
 
-check = county.groupby('state')['percent'].sum()
+grouped = county.groupby('state')
+
+check = grouped['percent'].sum()
 print( check )
+
+#%%
+#
+#  Demonstrate more features of groupby. First, trim the counties down to just 
+#  a couple of states for readability. We'll talk more about the .query() 
+#  method later in the semester.
+#
+
+trimmed = county.query("state =='04' or state == '36'")
+
+#
+#  Now group the counties by state and iterate through the groups to show 
+#  their structure. Not usually needed.
+#
+
+group_by_state = trimmed.groupby('state')
+
+for t,g in group_by_state:
+    print(f'\nGroup for type {t}:\n')
+    print(g)
+    
+#%%
+#
+#  Some quick aggregations, return one row per group
+#
+
+print( '\nMedian county population:' )
+pop_med = group_by_state['pop'].median()
+print( pop_med )
+
+print( '\n25th percentile county population:' )
+pop_25th = group_by_state['pop'].quantile(0.25)
+print( pop_25th )
+
+#%%
+#
+#  Some descriptive methods
+#
+
+print( '\nRows in each group:' )
+num_rows = group_by_state.size()
+print( num_rows )
+
+print( '\nPopulation statistics by group:' )
+inc_stats = group_by_state['pop'].describe()
+print( inc_stats )
+
+print( '\nUpper and lower population quartiles by group:')
+print( inc_stats[['25%','75%']])
+
+#%%
+#
+#  Selecting subsets of the records in each group. The dataframe was sorted 
+#  by population so the first and last rows of each group will be that group's
+#  smallest and largest populations.
+#
+
+print( '\nFirst row in each group:')
+first2 = group_by_state.head(2)
+print( first2 )
+
+print( '\nLast row in each group:')
+last2 = group_by_state.tail(2)
+print( last2 )
+
+#%%
+#
+#  The following could be used to find the largest populations if the 
+#  data were not already sorted. Returned largest to smallest within
+#  each group.
+#
+
+print( '\nChoosing rows with largest 2 populations in each group:')
+largest = group_by_state['pop'].nlargest(2)
+print( largest )
+
+#%%
+#
+#  Producing information about ungrouped data. These all return objects
+#  having the same index as the original data (i.e., 5 rows here with 
+#  index columns 'id' and 'type'.
+#
+
+print( '\nGroup numbers for raw data:')
+groups = group_by_state.ngroup()
+print( groups )
+
+print( '\nSequence within group:')
+seqnum = group_by_state.cumcount()
+print( seqnum )
+
+print( '\nCumulative population within group:')
+cumulative_inc = group_by_state['pop'].cumsum()
+print( cumulative_inc )
+
+print( '\nRank within group:')
+rank_age = group_by_state['pop'].rank()
+print( rank_age )
+
